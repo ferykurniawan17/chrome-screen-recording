26981833890a073be3016fb79e762325b6a68e81
fery kurniawan <ferykurniawan@ferys-MacBook-Pro.local>
2024-09-15 11:09:40 +0700

    init code

---
+++ background.js
@@ -0,0 +1,37 @@
+const startRecording = async () => {
+  console.log('====== START RECORDING 1');
+  await chrome.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': true}, async function (tabs) {
+    console.log('====== START RECORDING 2', tabs);
+    // Get current tab to focus on it after start recording on recording screen tab
+    const currentTab = tabs[0];
+
+    // Create recording screen tab
+    const tab = await chrome.tabs.create({
+      url: chrome.runtime.getURL('recording_screen.html'),
+      pinned: true,
+      active: true,
+    });
+
+    // Wait for recording screen tab to be loaded and send message to it with the currentTab
+    chrome.tabs.onUpdated.addListener(async function listener(tabId, info) {
+      if (tabId === tab.id && info.status === 'complete') {
+        chrome.tabs.onUpdated.removeListener(listener);
+
+        await chrome.tabs.sendMessage(tabId, {
+          name: 'startRecordingOnBackground',
+          body: {
+            currentTab: currentTab,
+          },
+        });
+      }
+    });
+  });
+};
+
+// Listen for startRecording message from popup.js
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log('============', request.name);
+  if (request.name === 'startRecording') {
+    startRecording();
+  }
+});
